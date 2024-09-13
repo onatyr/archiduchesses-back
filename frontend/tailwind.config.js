1436b4fb99468661e0cd83e2c590a443fb32c8e3
Claire Szt <claireszt@gmail.com>
2024-09-13 17:17:50 +0200

    frontend setup : vite, vue, ts, tailwind

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,28 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './index.html',
+    './src/**/*.{vue,js,ts,jsx,tsx}',
+  ],
+  theme: {
+    extend: {
+      colors: {
+        'apple': {
+        '50': '#f1f8ed',
+        '100': '#dff0d7',
+        '200': '#c1e3b3',
+        '300': '#9bd086',
+        '400': '#79bb60',
+        '500': '#5faa46',
+        '600': '#437f31',
+        '700': '#356229',
+        '800': '#2e4f25',
+        '900': '#294423',
+        '950': '#13240f',
+    },
+    
+      },
+    },
+  },
+  plugins: [],
+};
