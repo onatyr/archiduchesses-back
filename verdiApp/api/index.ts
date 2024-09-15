edf177753b3f582d569eb85c980cce73e8c6f46d
onatyr <onat.rigault@gmail.com>
2024-09-15 14:16:15 +0200

    dotenv.config added in database.ts, works fine

--- verdiApp/api/index.ts
+++ verdiApp/api/index.ts
@@ -2,17 +2,6 @@ import express from "express";
 import cors from "cors";
 import * as http from "http";
 import {router} from "./router";
-import {Pool} from "pg";
-import {drizzle} from "drizzle-orm/node-postgres";
-import dotenv from 'dotenv';
-
-dotenv.config()
-
-console.log('DB_USER:', process.env.DB_USER);
-console.log('DB_HOST:', process.env.DB_HOST);
-console.log('DB_NAME:', process.env.DB_NAME);
-console.log('DB_PASSWORD:', process.env.DB_PASSWORD);  // Check the type and value
-console.log('DB_PORT:', process.env.DB_PORT);
 
 const app = express.Router();
 
