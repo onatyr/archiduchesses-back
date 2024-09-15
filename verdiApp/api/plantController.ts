80cb2d38693aafc8a3ae6f0fe18dbcf2aefa8e26
onatyr <onat.rigault@gmail.com>
2024-09-15 02:06:24 +0200

    wip connexion to db ECONNREFUSED

--- verdiApp/api/plantController.ts
+++ verdiApp/api/plantController.ts
@@ -5,7 +5,13 @@ import {plants} from "../database/schema";
 export const plantController: express.Router = express();
 
 plantController.get("/all", async (req, res) => {
-    console.log(await db.select().from(plants));
     console.log("toutes les plantes")
+    try {
+        let allPlants = await db.select().from(plants).execute()
+    console.log(allPlants);
+
+    } catch (e) {
+        console.log(e)
+    }
     res.status(200).json()
 });
