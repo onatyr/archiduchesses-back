eb5f31de16def09f3dd129bc3ee87ffdfff04563
onatyr <onat.rigault@gmail.com>
2024-09-15 14:16:15 +0200

    dotenv.config added in database.ts, works fine

--- verdiApp/database/database.ts
+++ verdiApp/database/database.ts
@@ -1,26 +1,11 @@
 import {Pool} from "pg";
 import {drizzle} from "drizzle-orm/node-postgres";
+import dotenv from "dotenv";
+
+dotenv.config()
 
 export const pool = new Pool({
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    port: parseInt(process.env.DB_PORT || '5432'),
-    database: process.env.DB_NAME,
-    host: process.env.DB_HOST
+    connectionString: process.env.DB_URL
 });
-
-const testConnection = async () => {
-    try {
-        const client = await pool.connect();
-        console.log(client)
-        const res = await client.query('SELECT NOW()');
-        console.log('Connection successful:', res.rows);
-        client.release();
-    } catch (error) {
-        console.error('Connection failed:', error);
-    }
-};
-
-testConnection();
 
 export const db = drizzle(pool);
