62b10e20a00b944d2c95f10269b21025597ac1f8
onatyr <onat.rigault@gmail.com>
2024-09-18 22:41:03 +0200

    wip/ login route, plantController post /new doesn't work

---
+++ apps/api/controllers/plant/plantController.ts
@@ -0,0 +1,25 @@
+import express from "express";
+import {db} from "../../database/database";
+import {plants} from "../../database/schema";
+import { Plant } from '@shared/models/plant.model';
+
+export const plantController: express.Router = express();
+
+plantController.get("/all", async (req, res) => {
+    const allPlants = await db.select().from(plants).execute();
+    res.status(200).json(allPlants);
+});
+
+// plantController.post("/new", async (req: express.Request<unknown, unknown, Plant>, res) => {
+//     console.log('req body:', req.body);
+//     const plant = await db.insert(plants).values({
+//         userId: req.body.userId,
+//         name: req.body.name,
+//         family: req.body.family,
+//         sunlight: req.body.sunlight,
+//         watering: req.body.watering,
+//         adoptionDate: req.body.adoptionDate
+//     });
+//     console.log(plant)
+//     res.status(201).json()
+// }) // todo resolve type mismatch
