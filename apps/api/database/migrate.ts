61627be7176e8da5d107abcda23bebf25ca995bd
Claire Szt <claireszt@gmail.com>
2024-09-18 12:41:26 +0200

    fixed backend .env connexion

--- apps/api/database/migrate.ts
+++ apps/api/database/migrate.ts
@@ -1,14 +1,23 @@
-import 'dotenv/config';
 import postgres from "postgres";
 import {migrate} from "drizzle-orm/postgres-js/migrator";
 import {drizzle} from "drizzle-orm/postgres-js";
+import * as dotenv from 'dotenv';
+import path from 'path';
 
-const migrationClient = postgres(process.env.DB_URL!, { max: 1 });
+dotenv.config({
+    path: path.resolve(__dirname, '../../../.env') 
+});
+
+if (!process.env.DB_URL) {
+    throw new Error("DB_URL is not defined. Check your .env file or dotenv path.");
+}
+
+const migrationClient = postgres(process.env.DB_URL, { max: 1 });
 const db = drizzle(migrationClient)
 
 const main = async () => {
     console.log("Migrating database...");
-    await migrate(db, { migrationsFolder: "./verdiApp/database/migrations" });
+    await migrate(db, { migrationsFolder: "./migrations" });
     await migrationClient.end();
     console.log("Database migrated successfully!");
 };
