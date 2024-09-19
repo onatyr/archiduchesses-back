88353b5aa854f9385dba9b92ec235f7da9e3f2ac
onatyr <onat.rigault@gmail.com>
2024-09-19 15:15:27 +0200

    plantController now filter by req.userId

--- apps/api/controllers/plant/plantController.ts
+++ apps/api/controllers/plant/plantController.ts
@@ -2,17 +2,20 @@ import express from "express";
 import {db} from "../../database/database";
 import {plants} from "../../database/schema";
 import {Plant} from '@shared/models/plant.model';
+import {eq} from "drizzle-orm";
 
 export const plantController: express.Router = express();
 
 plantController.get("/all", async (req, res) => {
-    const allPlants = await db.select().from(plants).execute();
+    const allPlants = await db.select().from(plants)
+        .where(eq(plants.userId, req.userId))
+        .execute();
     res.status(200).json(allPlants);
 });
 
 plantController.post("/new", async (req: express.Request<unknown, unknown, Plant>, res) => {
     const plant = await db.insert(plants).values({
-        userId: req.body.userId,
+        userId: req.userId,
         name: req.body.name,
         family: req.body.family,
         sunlight: req.body.sunlight,
