2bbe86e8448bd8ada3b66379c8f7a736be053ba1
Claire Szt <claireszt@gmail.com>
2024-09-19 15:40:00 +0200

    WIP

--- apps/verdiApp/src/components/Welcome.vue
+++ apps/verdiApp/src/components/Welcome.vue
@@ -1,13 +1,21 @@
 <script lang="ts">
-import { ref } from 'vue';
-
-const names: string[] = ['Claire', 'Onat'];
+import { ref, onMounted } from 'vue';
 
 export default {
   setup() {
-    const randomName = ref<string>(names[Math.floor(Math.random() * names.length)]);
+    const userName = ref<string>('');
 
-    return { randomName };
+    onMounted(async () => {
+      try {
+        const response = await fetch('http://localhost:3000/user/name');
+        const data = await response.json();
+        userName.value = data;
+      } catch (error) {
+        console.error(error);
+      }
+    });
+
+    return { userName };
   },
 };
 </script>
@@ -16,7 +24,7 @@ export default {
     <div class="flex flex-col sm:flex-row justify-center items-center gap-4 p-6 bg-apple-100 w-full">
       <img src="/plant.png" class="w-16 h-16 sm:w-20 sm:h-20" alt="Plant icon" />
       <h1 class="text-center text-black font-extrabold text-3xl sm:text-4xl md:text-5xl uppercase">
-        Welcome, {{ randomName }}!
+        Welcome, {{ userName }}!
       </h1>
   </div>
 </template>
