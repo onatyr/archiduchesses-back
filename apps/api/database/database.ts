89f243a7e2eeed31d263a85f074453d677a90b57
onatyr <onat.rigault@gmail.com>
2024-10-05 00:11:17 +0200

    corrected gitignore

---
+++ apps/api/database/database.ts
@@ -0,0 +1,18 @@
+import { Pool } from "pg";
+import { drizzle } from "drizzle-orm/node-postgres";
+import dotenv from "dotenv";
+import path from 'path';
+
+dotenv.config({
+    path: path.resolve(__dirname, '../../../.env') 
+});
+
+if (!process.env.DB_URL) {
+    throw new Error("DB_URL is not defined. Check your .env file or dotenv path.");
+}
+
+export const pool = new Pool({
+    connectionString: process.env.DB_URL,
+});
+
+export const db = drizzle(pool);
