f60dc68b8be76119510ebf14fb221ff0a4f94fe0
onatyr <onat.rigault@gmail.com>
2024-10-05 11:12:48 +0200

    changed migration script

--- apps/api/database/schema.ts
+++ apps/api/database/schema.ts
@@ -29,5 +29,4 @@ export const plants = pgTable('plants', {
     watering: wateringEnum('watering'),
     adoptionDate: timestamp('adoption_date').defaultNow(),
     placeId: uuid('place_id').references(() => places.id),
-    test: varchar('test')
 });
