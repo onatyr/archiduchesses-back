5a7a000259e2c2f5ffe7800d196697f69371e060
onatyr <onat.rigault@gmail.com>
2024-10-05 02:14:08 +0200

    test 2

--- apps/api/database/schema.ts
+++ apps/api/database/schema.ts
@@ -29,5 +29,6 @@ export const plants = pgTable('plants', {
     watering: wateringEnum('watering'),
     adoptionDate: timestamp('adoption_date').defaultNow(),
     placeId: uuid('place_id').references(() => places.id),
-    test: varchar('test')
+    test: varchar('test'),
+    youhou: varchar('youhou')
 });
