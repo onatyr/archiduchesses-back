00f6cb76dea25374f1b0c4f3f0bd378b93359ada
onatyr <onat.rigault@gmail.com>
2024-10-06 12:43:15 +0200

    vite server config

--- apps/verdiApp/vite.config.ts
+++ apps/verdiApp/vite.config.ts
@@ -1,26 +1,30 @@
-import { fileURLToPath, URL } from 'node:url'
+import {fileURLToPath, URL} from 'node:url'
 
-import { defineConfig } from 'vite'
+import {defineConfig} from 'vite'
 import vue from '@vitejs/plugin-vue'
 import checker from "vite-plugin-checker";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-    checker({
-      typescript: true,
-    }),
-  ],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src/', import.meta.url)),
-      '@shared': fileURLToPath(new URL('../shared/', import.meta.url))
+    plugins: [
+        vue(),
+        checker({
+            typescript: true,
+        }),
+    ],
+    resolve: {
+        alias: {
+            '@': fileURLToPath(new URL('./src/', import.meta.url)),
+            '@shared': fileURLToPath(new URL('../shared/', import.meta.url))
+        },
+        extensions: ['.ts', '.tsx', '.json']
     },
-    extensions: ['.ts', '.tsx', '.json']
-  },
-  build: {
-    emptyOutDir: true, // Clear the output directory before building (default is true)
-    outDir: '../../dist/verdiApp', // Output directory (default is 'dist')
-  }
+    build: {
+        emptyOutDir: true, // Clear the output directory before building (default is true)
+        outDir: '../../dist/verdiApp', // Output directory (default is 'dist')
+    },
+    server: {
+        host: '0.0.0.0',
+        port: 5173
+    }
 })
