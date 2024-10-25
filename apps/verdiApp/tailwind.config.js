917760e1ccb0fc86b2ed574c97fbaf9cb4a979c5
Claire Szt <claireszt@gmail.com>
2024-10-25 15:51:36 +0200

    tailwind theme colors

--- apps/verdiApp/tailwind.config.js
+++ apps/verdiApp/tailwind.config.js
@@ -4,12 +4,34 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        matcha: '#909671',
-        pistache: '#B3B792',
-        carob: '#725C3A',
-        almond: '#E5E0D8',
-        chai: '#D2AB80',
-        vanilla: '#E5D2B8',
+        primary: '#809671',
+        primaryVariant: '#6A7A5B',
+        secondary: '#F1A500',
+        secondaryVariant: '#C96500',
+        background: '#FFFBF3',
+        surface: '#FFF5E4',
+        error: '#CA2D28',
+        onPrimary: '#FFFFFF',
+        onSecondary: '#FFFFFF',
+        onBackground: '#000000',
+        onSurface: '#000000',
+        onError: '#FFFFFF',
+        highlight: '#E5D2B8',
+        dark: {
+          primary: '#6A7A5B', // Darker variant of primary
+          primaryVariant: '#4E5A43',
+          secondary: '#C96500',
+          secondaryVariant: '#9A4900',
+          background: '#3A2F22', // Dark theme background color
+          surface: '#4F4233', // Dark theme surface color
+          error: '#CA2D28',
+          onPrimary: '#FFFFFF',
+          onSecondary: '#FFFFFF',
+          onBackground: '#E5E0D8', // Lightened for contrast on dark background
+          onSurface: '#D2AB80',
+          onError: '#E5E0D8',
+          highlight: '#D2AB80',
+        },
       },
     },
   },
