27158d008e99e426eba4556c3935eff82197af8e
onatyr <onat.rigault@gmail.com>
2024-11-15 16:40:39 +0100

    using import meta instead of dotenv config

--- apps/plant-app/vite.config.ts
+++ apps/plant-app/vite.config.ts
@@ -1,9 +1,15 @@
-import { defineConfig } from 'vite';
+import {defineConfig, loadEnv} from 'vite';
 import react from '@vitejs/plugin-react';
+import * as path from "node:path";
+import dotenv from 'dotenv';
 
 export default defineConfig({
-  plugins: [react()],
-  build: {
-    outDir: '../../dist/plant-app',
-  },
+
+    plugins: [react()],
+    define: {
+        'process.env': dotenv.config({ path: path.resolve(__dirname, '../../.env') })
+    },
+    build: {
+        outDir: '../../dist/plant-app',
+    },
 });
