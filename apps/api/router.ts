7804e86c90ded2a8105f11fa4761418e97aaae78
onatyr <onat.rigault@gmail.com>
2024-11-17 20:06:54 +0100

    route allRoomsByPlaceId

--- apps/api/router.ts
+++ apps/api/router.ts
@@ -1,15 +1,12 @@
 import express from 'express';
-// import { testController } from './test.controller';
 import { plantController } from './controllers/plant/plant.controller';
 import { authController } from './controllers/auth/auth.controller';
 import { userController } from './controllers/user/user.controller';
+import { placeController } from "./controllers/places/place.controller";
 
 export const router: express.Router = express.Router();
 
-// router.use('/hey', testController);
-
 router.use('/auth', authController);
-
 router.use('/user', userController);
-
 router.use('/plant', plantController);
+router.use('/place', placeController)
