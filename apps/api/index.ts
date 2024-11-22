0236304d6a36581c0b733dfe8c7353c90beef409
onatyr <onat.rigault@gmail.com>
2024-11-22 11:05:09 +0100

    rm unused function

--- apps/api/index.ts
+++ apps/api/index.ts
@@ -18,9 +18,6 @@ const PORT = 3000;
 // Middleware
 app.use(cors());
 app.use(express.json({ limit: '10mb' }));
-app.use((req: express.Request, res: express.Response, next: NextFunction) => {
-  next();
-});
 app.use(authenticate);
 
 // Controllers
