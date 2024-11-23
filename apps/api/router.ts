82aacd0d40999b4a253b98730faff2e10044d84c
onatyr <onat.rigault@gmail.com>
2024-11-23 13:38:52 +0100

    Revert "plant.post /id filter out images that are not jpeg or png, image buffer made with readFileSync"
    This reverts commit 85f92d85535a7c0e318859bfc5183507a8f45cc0.

--- apps/api/router.ts
+++ apps/api/router.ts
@@ -1,9 +1,12 @@
 import express from 'express';
+// import { testController } from './test.controller';
 import { plantController } from './controllers/plant/plant.controller';
 import { authController } from './controllers/auth/auth.controller';
 import { userController } from './controllers/user/user.controller';
 
 export const router: express.Router = express.Router();
+
+// router.use('/hey', testController);
 
 router.use('/auth', authController);
 
