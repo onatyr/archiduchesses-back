295b86c8c1ac28bb26cb8d1025eccede2753d3de
onatyr <onat.rigault@gmail.com>
2024-12-14 04:40:42 +0100

    cleared js files

--- apps/api/controllers/tasks/tasks.util.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-function computeNextOccurrence(recurrenceInDays) {
-    const currentDate = new Date();
-    currentDate.setDate(currentDate.getDate() + recurrenceInDays);
-    return currentDate;
-}
